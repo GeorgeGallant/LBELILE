db51b2aa0d5840faa29bf800b1cfb4f30d00a343
Matthew Willox <skintilla@gmail.com>
2022-01-06 12:26:17 -0700

    Convert remote script to work with XR toolkit

--- Assets/Scripts/Remotescript.cs
+++ Assets/Scripts/Remotescript.cs
@@ -2,25 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using Valve.VR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Remotescript : MonoBehaviour
 {
     public string nextSceneName;
-    public Transform hand;
     public Vector3 prefPos; 
     public Vector3 prefRotation;
-    public SteamVR_Action_Boolean TV;
-    public SteamVR_Input_Sources handType;
     public float timer;
     public bool useTimer;
 
+    public ButtonHandler buttonPress;
+
+    public void OnDestroy()
+    {
+        buttonPress.OnButtonDown -= Clicked;
+    }
+
     private void Start()
     {
-        hand = GameObject.FindWithTag("Attach").transform;
-        //find the player hand and attach
-        this.gameObject.transform.SetParent(hand);
-        //this is to change the remot so it look correct in the players hand
         this.transform.localPosition = prefPos;
         this.transform.localRotation = Quaternion.Euler(prefRotation);
         if (!useTimer) {
@@ -42,10 +42,10 @@ public class Remotescript : MonoBehaviour
     //a methode to cue once the player is good to press the button
     public void goodToPress()
     {
-        TV.AddOnStateDownListener(Clicked, handType);
+        buttonPress.OnButtonDown += Clicked;
     }
    
-    private void Clicked(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    private void Clicked(XRController controller)
     {
         //de attach from hand so it does not go the next scene
         this.gameObject.transform.SetParent(null);
