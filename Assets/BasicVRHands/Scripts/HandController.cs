dbf0c392cf2a207c80c601cccc18f23df5e4555d
Matthew Willox <skintilla@gmail.com>
2022-01-06 12:25:43 -0700

    Hand adjustments to work with XR toolkit

--- Assets/BasicVRHands/Scripts/HandController.cs
+++ Assets/BasicVRHands/Scripts/HandController.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class HandController : MonoBehaviour {
 
     private Animator animator;
 
+	public ButtonHandler buttonPress;
+    public string controllerName;
+	public bool down;
+
 	void Start () {
         animator = GetComponent<Animator>();
+		buttonPress.OnButtonDown += OnDown;
+		buttonPress.OnButtonUp += OnUp;
 	}
-	
-	void Update () {
-        animator.SetBool("isGrabbing", Input.GetKey(KeyCode.F));
+    private void OnDestroy()
+    {
+        buttonPress.OnButtonDown -= OnDown;
+        buttonPress.OnButtonUp -= OnUp;
+    }
+
+    private void OnDown(XRController controller)
+    {
+        if(controller.name == controllerName)
+        {
+            Debug.Log(controller.name+"DOWN");
+            down = true;
+        }
+        
+    }
+
+    private void OnUp(XRController controller)
+    {
+        if (controller.name == controllerName)
+        {
+            Debug.Log(controller.name + "UP");
+            down = false;
+        }
+    }
+
+    void Update () {
+        animator.SetBool("isGrabbing", down);
 	}
 }
