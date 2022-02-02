9d396582adece4c5b12619246fdbfc717f1469a4
Matthew Willox <matthew@cybermancers.ca>
2022-02-01 21:17:59 -0700

    Use blend tree instead of animation transitions

--- Assets/HandAnimator.cs
+++ Assets/HandAnimator.cs
@@ -12,26 +12,27 @@ public class HandAnimator : MonoBehaviour
     bool grab = false;
     bool lastPressedTrigger = false;
     bool lastPressedGrab = false;
+    float blendTarget = 0f;
+    float blendAmount = 0f;
+    public float blendRate = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        GameObject parentObj = gameObject.transform.parent.gameObject.transform.parent.gameObject;
-        Debug.Log(parentObj.name);
-        if(parentObj != null)
-        {
-            Debug.Log("Found Controller Parent");
-            controller = parentObj.GetComponent<XRController>();
-            Debug.Log(controller);
-        }
-         
-        
+        controller = gameObject.GetComponentInParent<XRController>();
+
+        Debug.Log(controller.name);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(controller.name + controllerName);    
+
+        blendAmount += (blendTarget - blendAmount) * blendRate;
+
+        animator.SetFloat("Blend", blendAmount);
+
         if (controller && controller.name == controllerName)
         {
 
@@ -53,29 +54,32 @@ public class HandAnimator : MonoBehaviour
 
             if (grab && trigger)
             {
-                Debug.Log("Closed Hand");
-                animator.Play("Base Layer.Index Close", 0, 2f);
+                //Debug.Log("Closed Hand");
+                blendTarget = 1f;
                 return;
             }
 
             if (trigger)
             {
-                Debug.Log("Press Trigger");
-                animator.Play("Base Layer.Index Close", 0, 2f);
+                //Debug.Log("Press Trigger");
+                blendTarget = 1f;
+                //animator.Play("Base Layer.Index Close", 0, 2f);
                 return;
             }
 
             if (grab)
             {
-                Debug.Log("Press Grip");
-                animator.Play("Base Layer.Index Point", 0, 2f);
+                //Debug.Log("Press Grip");
+                blendTarget = 0.5f;
+                //animator.Play("Base Layer.Index Point", 0, 2f);
                 return;
             }
 
             if (!grab && !trigger)
             {
-                Debug.Log("Open Hand");
-                animator.Play("Base Layer.Idle", 0, 2f);
+                //Debug.Log("Open Hand");
+                blendTarget = 0f;
+                //animator.Play("Base Layer.Idle", 0, 2f);
             }
         }
 
