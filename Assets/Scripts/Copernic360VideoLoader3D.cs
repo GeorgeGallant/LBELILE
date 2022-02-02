0975342fb5af3b17179ee5f81071f689b12344cd
GeorgeGallant <40878690+GeorgeGallant@users.noreply.github.com>
2022-02-02 16:31:58 -0700

    Install the rest of the Copernic360

---
+++ Assets/Scripts/Copernic360VideoLoader3D.cs
@@ -0,0 +1,30 @@
+﻿using Kage.Copernic360;
+using Kage.Copernic360.Configuration;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Video;
+
+
+    public class Copernic360VideoLoader3D : MonoBehaviour
+    {
+        [SerializeField] private VideoPlayer videoPlayer;
+
+        private SkyboxController skyboxController;
+
+        private void Awake()
+        {
+            string configPath = Path.Combine(Application.streamingAssetsPath,"3D.6dof");
+            using (var fs = new FileStream(configPath,FileMode.Open, FileAccess.Read))
+            {
+                skyboxController = new SkyboxController(videoPlayer.targetTexture,VideoConfiguration.LoadFromStream(fs)
+                );
+            }
+        }
+        private void Update()
+        {
+            long frame = videoPlayer.frame;
+            if (frame < 0)
+                return;
+            skyboxController.SetCurrentFrame((ulong)frame);
+        }
+    }
