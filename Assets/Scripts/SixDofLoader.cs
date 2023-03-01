9eb82aaa4c0e027250eaf2d4c477975e86ac4fd9
Matthew Willox <matthew@cybermancers.ca>
2023-03-01 12:02:47 -0700

    create 6dof loader
    6dof loader will read the filename for the video file and load the corresponding .6dof file from the same folder

---
+++ Assets/Scripts/SixDofLoader.cs
@@ -0,0 +1,32 @@
+﻿using Kage.Copernic360;
+using Kage.Copernic360.Configuration;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Video;
+
+
+    public class SixDofLoader : MonoBehaviour
+    {
+        [SerializeField] private VideoPlayer videoPlayer;
+        [SerializeField] private VideoLoader videoLoader;
+
+        private SkyboxController skyboxController;
+
+        private void Awake()
+        {
+            string configPath = Path.Combine(Application.persistentDataPath, videoLoader.fileName + ".6dof");
+            Debug.Log("LOADING 6DOF");
+            Debug.Log(configPath);
+            using (var fs = new FileStream(configPath,FileMode.Open, FileAccess.Read))
+            {
+                skyboxController = new SkyboxController(videoPlayer.targetTexture,VideoConfiguration.LoadFromStream(fs));
+            }
+        }
+        private void Update()
+        {
+            long frame = videoPlayer.frame;
+            if (frame < 0)
+                return;
+            skyboxController.SetCurrentFrame((ulong)frame);
+        }
+    }
