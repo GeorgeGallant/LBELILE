04cb353a786926dc505eb9cb9b452a4e580ede97
Matthew Willox <matthew@cybermancers.ca>
2023-03-31 09:46:18 -0600

    handle menu button press

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class GameOver : MonoBehaviour
 {
@@ -14,8 +15,11 @@ public class GameOver : MonoBehaviour
 
     public string ReplayDestination = "Opening";
     public string RestartDestination = null;
+    public ButtonHandler buttonPress;
+    public bool buttonPressed;
+    public GameObject[] showOnEnable;
 
-    void Start()
+    void Awake()
     {
         Content.text = Message;
         if(RestartDestination == null)
@@ -23,16 +27,54 @@ public class GameOver : MonoBehaviour
             Scene scene = SceneManager.GetActiveScene();
             RestartDestination = scene.name;
         }
+        BindXRControllerEvents();
+        gameObject.SetActive(buttonPressed);
+        foreach (GameObject go in showOnEnable)
+        {
+            go.SetActive(false);
+        }
+    }
+
+    public void OnDestroy()
+    {
+        UnbindXRControllerEvents();
+    }
+
+    public void BindXRControllerEvents()
+    {
+        Debug.Log("bind");
+        buttonPress.OnButtonDown += ButtonDown;
+
+    }
+
+    public void UnbindXRControllerEvents()
+    {
+        if (buttonPress)
+        {
+            buttonPress.OnButtonDown -= ButtonDown;
+        }
+    }
+
+    public void ButtonDown(XRController controller, InputHelpers.Button button)
+    {
+        buttonPressed = !buttonPressed;
+        gameObject.SetActive(buttonPressed);
+        foreach(GameObject go in showOnEnable)
+        {
+            go.SetActive(buttonPress);
+        }
     }
 
     public void ResetGame()
     {
-        SceneManager.LoadScene(RestartDestination);
+        Debug.Log("Load scene " + ReplayDestination);
+        ConfigManager.Instance.goToScene(RestartDestination);
     }
 
     public void RestartScene()
     {
-        SceneManager.LoadScene(RestartDestination);
+        Debug.Log("Restart scene " + RestartDestination);
+        ConfigManager.Instance.goToScene(RestartDestination);
     }
 
 
